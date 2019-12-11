3f49352b5fd853c7f308c363a1a8f4906f948650
Darko Kezic <dkezic@yahoo.com>
2019-12-11 18:17:28 +0100

    Add examples

--- main.go
+++ main.go
@@ -11,27 +11,71 @@ import (
 	"github.com/Pallinder/go-randomdata"
 	_ "github.com/lib/pq"
 	"github.com/volatiletech/sqlboiler/boil"
+	"github.com/volatiletech/sqlboiler/queries/qm"
 )
 
 func main() {
 
+	// Connect to database
 	db, err := sql.Open("postgres", "dbname=airplanes user=postgres password=postgres")
-	if err != nil {
-		panic(err)
-	}
+	dieIf(err)
 	defer db.Close()
 
 	err = db.Ping()
-	if err != nil {
-		panic(err)
-	}
+	dieIf(err)
 
+	// Insert new pilot
 	p := &models.Pilot{Name: randomdata.LastName()}
 	err = p.Insert(context.Background(), db, boil.Infer())
+	dieIf(err)
+	fmt.Printf("Pilot ID: %v Name: %v\n", p.ID, p.Name)
+
+	// Get one pilot
+	onePilot, err := models.Pilots().One(context.Background(), db)
+	dieIf(err)
+	fmt.Printf("One pilot: %+v\n", onePilot)
+
+	// Found pilot on ID
+	f, err := models.FindPilot(context.Background(), db, onePilot.ID)
+	dieIf(err)
+	fmt.Printf("Found pilot: %+v\n", f)
+
+	// Update pilot
+	f.Name = randomdata.LastName()
+	rows, err := f.Update(context.Background(), db, boil.Whitelist(models.PilotColumns.Name))
+	dieIf(err)
+	fmt.Printf("Rows: %+v\n", rows)
+
+	// SELECT pilot
+	fa, err := models.Pilots(qm.Where("name = ?", f.Name)).One(context.Background(), db)
+	dieIf(err)
+	fmt.Printf("Found where: %+v\n", fa)
+
+	// Check if pilot exists
+	exists, err := models.PilotExists(context.Background(), db, fa.ID)
+	dieIf(err)
+	fmt.Printf("Pilot exists: %+v\n", exists)
+
+	// Count number of records in table
+	count, err := models.Pilots().Count(context.Background(), db)
+	dieIf(err)
+	fmt.Printf("Number of pilots: %+v\n", count)
+
+	// Get first pilot in table, and show languages and jets for this pilot
+	op, err := models.Pilots(qm.Where("id = ?", 1)).One(context.Background(), db)
+	dieIf(err)
+	languages, err := op.Languages().All(context.Background(), db)
+	dieIf(err)
+	fmt.Printf("Pilot %v knows following lanugages:\n", op.Name)
+	for _, oneLang := range languages {
+		fmt.Printf("%v\n", oneLang.Language)
+	}
+
+}
+
+func dieIf(err error) {
 	if err != nil {
 		panic(err)
 	}
-
-	fmt.Printf("Pilot ID: %v Name: %v\n", p.ID, p.Name)
 
 }
